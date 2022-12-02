8ce43764bd0f360b7cb9d3aef81c3c6e1127f977
Abdelhay-Ali <118073201+Abdelhay-Ali@users.noreply.github.com>
2022-12-02 22:57:58 +0200

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/sbin/sh
+
+
+
+# Example:
+
+
+# ui_print "Applying update..."
+#
+# package_extract_file newbinary $INSTALLDIR/newbinary
+#
+# mv $INSTALLDIR/newbinary /system/bin/newbinary
+#
+# set_metadata /system/bin/newbinary uid root gid shell mode 755
+#
+# 
+# ui_print "Update successfully installed!"
+
+
